21bafe41ab14e20cd47e7d4b34ff98a126df3cea
raphael lebas <raphael.lebas@outlook.fr>
2020-05-06 11:07:20 +0200

    Initial Commit

---
+++ ProjetDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjetDemo
+//
+//  Created by Raphael Lebas on 06/05/2020.
+//  Copyright © 2020 Raphael Lebas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
